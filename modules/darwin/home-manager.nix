aa9539962fe4a399437178685c5599912ebd30ec
James Wyse <james@jameswyse.net>
2024-01-11 14:21:34 +1000

    move 1pwd

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -45,9 +45,7 @@ in
 
         stateVersion = "23.11";
       };
-      programs = {
-          _1password-gui.enable = true;
-      } // import ../shared/home-manager.nix { inherit config pkgs lib; };
+      programs = {} // import ../shared/home-manager.nix { inherit config pkgs lib; };
 
       # Marked broken Oct 20, 2022 check later to remove this
       # https://github.com/nix-community/home-manager/issues/3344
