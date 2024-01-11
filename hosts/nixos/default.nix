f66bb6e0abdf55b18ce3555758d06fd208858663
James Wyse <james@jameswyse.net>
2024-01-11 12:19:07 +1000

    Update default.nix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -59,12 +59,6 @@ in
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
-    gc = {
-      user = "root";
-      automatic = true;
-      interval = { Weekday = 0; Hour = 2; Minute = 0; };
-      options = "--delete-older-than 30d";
-    };
    };
 
   # Manages keys and such
