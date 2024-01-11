2a6dd59a025eff6fc11dec2ad2eaa7d02be1de44
James Wyse <james@jameswyse.net>
2024-01-11 12:35:15 +1000

    Update default.nix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -136,13 +136,13 @@ in
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   jack.enable = true;
+  # };
 
   hardware = {
     enableRedistributableFirmware = lib.mkDefault true;
