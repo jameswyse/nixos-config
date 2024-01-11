5f5cef3ccfbd729014e8a0e1c2a5de0797fec0b5
James Wyse <james@jameswyse.net>
2024-01-11 12:58:13 +1000

    update filesystems

--- modules/nixos/disk-config.nix
+++ modules/nixos/disk-config.nix
@@ -1,34 +1,47 @@
 _: {
   # This formats the disk with the ext4 filesystem
   # Other examples found here: https://github.com/nix-community/disko/tree/master/example
-  disko.devices = {
-    disk = {
-      vdb = {
-        device = "/dev/%DISK%";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "100M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
+  # disko.devices = {
+  #   disk = {
+  #     vdb = {
+  #       device = "/dev/%DISK%";
+  #       type = "disk";
+  #       content = {
+  #         type = "gpt";
+  #         partitions = {
+  #           ESP = {
+  #             type = "EF00";
+  #             size = "100M";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "vfat";
+  #               mountpoint = "/boot";
+  #             };
+  #           };
+  #           root = {
+  #             size = "100%";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "ext4";
+  #               mountpoint = "/";
+  #             };
+  #           };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/cb428431-0d91-486f-a8b4-d9ab1b89f0fc";
+      fsType = "ext4";
     };
-  };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/4795-59E2";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/360e63e6-d0e0-422f-8cac-3db30347d1d2"; }
+    ];
 }
