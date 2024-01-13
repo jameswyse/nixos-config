014b9015d88e2550a59b5f48125934f929740c4e
James Wyse <james@jameswyse.net>
2024-01-13 14:58:47 +1000

    Update default.nix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,4 +1,4 @@
-{ config, inputs, lib, pkgs, agenix, ... }:
+{ config, inputs, lib, pkgs, nix-vscode-extensions, agenix, ... }:
 
 let user = "james";
     keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ]; 
